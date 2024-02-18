4125a2006bcd877d387af3af9d25cfb455695dd8
OriGuyUniqueDev <origuydev@gmail.com>
2024-02-18 20:48:33 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	root: "./",
+	build: {
+		outDir: "dist",
+	},
+	resolve: {
+		alias: {
+			"@": "/src",
+		},
+	},
+	optimizeDeps: {
+		exclude: ["js-big-decimal"],
+	},
+});
